d24ad3e86d1e9b38dd15902ed2ccd26ada0f2486
Jason Mobarak <jam@cozybit.com>
2013-07-01 15:20:48 -0700

    gitreview script and updated bash aliases

---
+++ scripts/gitreview.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for revid in `git cherry master|sed 's/\+ //'`; do
+    
+    git log --stat -1 $revid
+    echo
+
+    x=y
+    read -p "Diff? [Y/n] " x
+
+    [ x"$x" = x"" ] && x=y
+    [ x"$x" = x"y" ] && git difftool "$revid^!"
+    
+done
