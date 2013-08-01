bd085d612beffd397aff1a95b3c0d7688c807ead
Jason Mobarak <jam@cozybit.com>
2013-08-01 10:42:13 -0700

    add latest dot files and utility scripts

--- scripts/git-review.bash
+++ scripts/git-review.bash
@@ -9,6 +9,7 @@ for revid in `git cherry master|sed 's/\+ //'`; do
     read -p "Diff? [Y/n] " x
 
     [ x"$x" = x"" ] && x=y
+    #[ x"$x" = x"y" ] && git difftool -t gvimdiff "$revid^!"
     [ x"$x" = x"y" ] && git difftool "$revid^!"
     
 done
