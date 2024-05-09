ab340f4c8cdcd75556c801579588c880c562ddb3
Jason Mobarak <git@jason.mobarak.name>
2024-05-08 18:33:22 -0700

    updates

--- scripts/vc-toggle-speakers.sh
+++ scripts/vc-toggle-speakers.sh
@@ -4,10 +4,10 @@ speakers="alsa_output.usb-Creative_Technology_Ltd_Creative_Pebble_Pro_00023CA2B2
 headset="alsa_output.usb-046d_G435_Wireless_Gaming_Headset_V001008005.1-01.analog-stereo"
 
 if pactl get-default-sink | grep -q 'Creative.*Pebble.*Pro'; then
-	sink="Headset"
+	sink="🎧"
 	pactl set-default-sink $headset
 else
-	sink="Speakers"
+	sink="🔊"
 	pactl set-default-sink $speakers
 fi
 
@@ -16,8 +16,8 @@ fi
 app_name=
 replaces_id=0x9efcbbef
 app_icon=
-summary="Sound: $sink defaulted"
-body='The default sound output device was changed.'
+summary="🎶 <===> $sink <===> 🎶"
+body='default device updated'
 actions='[]'
 hints='{"urgency": <1>}'
 expire_timeout=5000
@@ -28,11 +28,11 @@ gdbus \
 	--dest=org.freedesktop.Notifications \
 	--object-path=/org/freedesktop/Notifications \
 	--method=org.freedesktop.Notifications.Notify \
-	$app_name \
-	$replaces_id \
-	$app_icon \
-	$summary \
-	$body \
-	$actions \
-	$hints \
-	$expire_timeout
+	"$app_name" \
+	"$replaces_id" \
+	"$app_icon" \
+	"$summary" \
+	"$body" \
+	"$actions" \
+	"$hints" \
+	"$expire_timeout"
