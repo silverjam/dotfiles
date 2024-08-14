58138cb70563cde8ebc7985516e8b2801b1234fe
Jason Mobarak <git@jason.mobarak.name>
2024-08-14 11:51:21 -0700

    formatting

--- lazyvim/lua/plugins/lazyvim.lua
+++ lazyvim/lua/plugins/lazyvim.lua
@@ -6,100 +6,100 @@ local machine = vim.loop.os_gethostname()
 local colorscheme = "desert"
 
 if machine == "ganymede" then
-	--colorscheme = "catppuccin-frappe"
-	colorscheme = "rose-pine-moon"
+  --colorscheme = "catppuccin-frappe"
+  colorscheme = "rose-pine-moon"
 elseif machine == "ramjet" then
-	colorscheme = "gruvbox"
+  colorscheme = "gruvbox"
 end
 
 return {
-	{ "ellisonleao/gruvbox.nvim" },
-	{ "catppuccin/nvim" },
-	{ "rose-pine/neovim" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim" },
+  { "rose-pine/neovim" },
 
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = colorscheme,
-		},
-	},
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = colorscheme,
+    },
+  },
 
-	-- Confiugre neotest, add Python, Rust and Deno
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-neotest/neotest-python",
-			"nvim-neotest/neotest-plenary",
-			"rouge8/neotest-rust",
-			--"MarkEmmons/neotest-deno",
-		},
-		opts = {
-			adapters = {
-				"neotest-plenary",
-				"neotest-python",
-				--"neotest-deno",
-				"neotest-rust",
-			},
-		},
-	},
+  -- Confiugre neotest, add Python, Rust and Deno
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neotest/neotest-python",
+      "nvim-neotest/neotest-plenary",
+      "rouge8/neotest-rust",
+      --"MarkEmmons/neotest-deno",
+    },
+    opts = {
+      adapters = {
+        "neotest-plenary",
+        "neotest-python",
+        --"neotest-deno",
+        "neotest-rust",
+      },
+    },
+  },
 
-	-- Configure neo-tree, add line numbers
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		opts = {
-			event_handlers = {
-				{
-					event = "neo_tree_buffer_enter",
-					handler = function(_)
-						vim.opt.relativenumber = true
-						vim.opt.number = true
-					end,
-				},
-			},
-		},
-	},
+  -- Configure neo-tree, add line numbers
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      event_handlers = {
+        {
+          event = "neo_tree_buffer_enter",
+          handler = function(_)
+            vim.opt.relativenumber = true
+            vim.opt.number = true
+          end,
+        },
+      },
+    },
+  },
 
-	-- Config nvim-lint
-	{
-		"mfussenegger/nvim-lint",
-		opts = {
-			linters_by_ft = {
-				python = { "mypy" },
-			},
-		},
-	},
+  -- Config nvim-lint
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        python = { "mypy" },
+      },
+    },
+  },
 
-	-- Config telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		opts = {
-			pickers = {
-				live_grep = {
-					additional_args = { "--hidden", "--glob=!.git" },
-				},
-			},
-		},
-	},
+  -- Config telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      pickers = {
+        live_grep = {
+          additional_args = { "--hidden", "--glob=!.git" },
+        },
+      },
+    },
+  },
 
-	-- lsp-config
-	{
-		"neovim/nvim-lspconfig",
-		init_options = {
-			userLanguages = {
-				eelixir = "html-eex",
-				eruby = "erb",
-				rust = "html",
-			},
-		},
-		opts = {
-			servers = {
-				denols = {
-					autostart = false,
-				},
-			},
-		},
-	},
+  -- lsp-config
+  {
+    "neovim/nvim-lspconfig",
+    init_options = {
+      userLanguages = {
+        eelixir = "html-eex",
+        eruby = "erb",
+        rust = "html",
+      },
+    },
+    opts = {
+      servers = {
+        denols = {
+          autostart = false,
+        },
+      },
+    },
+  },
 }
