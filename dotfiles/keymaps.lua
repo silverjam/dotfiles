b2f9a1cb64eaab1804e94a89fb24156700ee7b61
Jason Mobarak <git@jason.mobarak.name>
2024-09-06 09:43:03 -0700

    update nvim keybinds

--- dotfiles/keymaps.lua
+++ dotfiles/keymaps.lua
@@ -5,6 +5,16 @@
 --
 --vim.keymap.set("n", "<space>dr", debugrun, { desc = "debug run" })
 
----- unbind the really annoying move line bindings
+---- Unbind the really annoying move line bindings
 vim.keymap.del({ "n", "i", "v" }, "<A-j>")
 vim.keymap.del({ "n", "i", "v" }, "<A-k>")
+
+---- Bind  <Space><Space> to search files in git
+---    (Default is to search "root dir" which can change in a mixed language project)
+vim.keymap.del("n", "<Space><Space>")
+vim.keymap.set(
+  "n",
+  "<Space><Space>",
+  ":Telescope git_files<CR>",
+  { remap = false, silent = true, desc = "Find in git (*s)" }
+)
